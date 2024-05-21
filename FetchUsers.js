af7777a9b22b844c7790182da7272431e7083da9
Mana <86923626+Mana42138@users.noreply.github.com>
2024-05-21 10:54:13 +0200

    Update FetchUsers.js

--- FetchUsers.js
+++ FetchUsers.js
@@ -1,7 +1,6 @@
 const URL = 'https://01e834c3-5d9f-4824-8069-2db58bbff81e-00-pjxee0fna3ai.kirk.replit.dev/api/v1/data.json';
 
-const GetUserData2 = () => {
-    return 'Hello world!!!!!!!!!';
+console.log("FetchUsers.js loaded");
+export const GetUserData2 = () => {
+    // Your function logic here
 };
-
-module.export = GetUserData2;
