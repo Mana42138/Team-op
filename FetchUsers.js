ea186f5c182ea724b29bcacdfb2eacde0a6e480f
Mana <86923626+Mana42138@users.noreply.github.com>
2024-05-21 10:47:49 +0200

    Create FetchUsers.js

---
+++ FetchUsers.js
@@ -0,0 +1,7 @@
+const URL = 'https://01e834c3-5d9f-4824-8069-2db58bbff81e-00-pjxee0fna3ai.kirk.replit.dev/api/v1/data.json';
+
+const GetUserData2 = () => {
+    return 'Hello world!!!!!!!!!';
+};
+
+module.export = GetUserData2;
